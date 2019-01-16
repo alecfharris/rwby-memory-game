f0dcf353285ce8949731996cb3e7da6f175924ec
Alec Harris <alecfharris@gmail.com>
2019-01-16 15:17:49 -0600

    Updates

---
+++ precache-manifest.66b3d5128adffb7258f22f8cd0e7d605.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f78a5f0225453f5d371b4a948986c18e",
+    "url": "/rwby-memory-game/static/media/rwby_style.f78a5f02.ttf"
+  },
+  {
+    "revision": "9fe657d90f4335a34d67",
+    "url": "/rwby-memory-game/static/js/runtime~main.9fe657d9.js"
+  },
+  {
+    "revision": "0ed3859abba78e09e2b7",
+    "url": "/rwby-memory-game/static/js/main.0ed3859a.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/rwby-memory-game/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "0ed3859abba78e09e2b7",
+    "url": "/rwby-memory-game/static/css/main.438b85d0.chunk.css"
+  },
+  {
+    "revision": "8c6a661d93bae7568461487889d4c47f",
+    "url": "/rwby-memory-game/index.html"
+  }
+];
